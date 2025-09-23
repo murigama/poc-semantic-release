7acb6d32b57bbc5c23b205ad712db8c45f422455
Luan Duarte <luan.duarte@superlogica.com>
2025-09-23 18:00:58 -0300

    feat: add teste5 log for semantic release

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ function main() {
   console.log('teste2')
   console.log('teste3')
   console.log('teste4')
+
+  console.log('teste5 - semantic-released')
 }
 
 // Executa a função principal se o arquivo for executado diretamente
